a4756742f74a11105dfc505543a632ae2690134d
basilshah <42776530+basilshah@users.noreply.github.com>
2021-05-04 11:45:57 +0530

    Update gomeet.py

--- gomeet.py
+++ gomeet.py
@@ -29,8 +29,8 @@ def gomet(num):
     driver.get('https://app.gotomeeting.com/index.html?meetingId='+num)
 
     time.sleep(3)
-    u = 2
-    while u< 3:
+    u = 1
+    while u< 2:
         try:
 
             body = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body')))
@@ -39,10 +39,10 @@ def gomet(num):
         except:
             driver.refresh()
         u+=1
-    
+
     time.sleep(3)
-    u = 2
-    while u <3 :
+    u = 1
+    while u <2 :
         try:
 
             body = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body')))
@@ -72,23 +72,31 @@ def gomet(num):
 
         a = driver.find_elements_by_xpath("//button[@type='button']")
         a[-1].click()
-    body = wait.until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, 'button[aria-label="Share microphone after join"]')))
-    try:
-        driver.find_element_by_xpath(
-            '''//input[@data-automation-id="change-name-and-email-dialog__edit-name"]''').send_keys(
-            'muhammed basil shah')
-        driver.find_element_by_xpath("//button[@type='submit']").click()
+    u = True
+    while u:
+        try:
+            #time.sleep(60)
+            body = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'button[aria-label="Share microphone after join"]')))
+            driver.find_element_by_xpath('''//input[@data-automation-id="change-name-and-email-dialog__edit-name"]''').send_keys('muhammed basil shah')
+            driver.find_element_by_xpath("//button[@type='submit']").click()
+            u = False
 
-        total_num = driver.find_elements_by_xpath("//button[@type='button']")[2]
-        tot = total_num.text.split('+')
-        totatt = tot[0]
-        total_number = int(totatt)
+        except:
+            time.sleep(900)
 
-        print(total_number)
-    except:
-        time.sleep(60)
-        pass
+    u =True
+    while u:
+        try:
+            total_num = driver.find_elements_by_xpath("//button[@type='button']")[2]
+            tot = total_num.text.split('+')
+            totatt = tot[0]
+            total_number = int(totatt)
+
+            print(total_number)
+            u = False
+        except:
+            time.sleep(60)
+            pass
 
 
     a = True
@@ -96,26 +104,30 @@ def gomet(num):
         try:
             time.sleep(3)
             current_coun = total_num.text
-            print(current_coun, end='')
-            print('txt')
+            print(current_coun)
         except:
             break
         try:
             current_count = int(current_coun)
             if current_count > total_number:
                 total_number = current_count
-            if current_count < 2:
-                print('get out')
-                driver.find_element_by_xpath('''//button[@aria-label="Leave"]''').click()
-                driver.find_element_by_xpath('''//buttton[@data-automation-id="attendee-leave-confirm"]''').click()
+            if current_count < 3:
+                driver.get('https://www.google.com/')
+                driver.__exit__()
+
                 break
-            if current_count < (total_number / 3):
-                driver.find_element_by_xpath('''//button[@aria-label="Leave"]''').click()
-                driver.find_element_by_xpath('''//buttton[@data-automation-id="attendee-leave-confirm"]''').click()
+            if current_count < (total_number / 2):
+                driver.get('https://www.google.com/')
+                driver.__exit__()
                 break
         except:
             pass
     driver.__exit__()
+
+
+
+
+
 
 
 
